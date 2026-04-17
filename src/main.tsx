38e1e6b2fa09f4642b165e5cef09411f1858fade
Marcus Xavier <marcusxavierr123@gmail.com>
2026-04-17 11:41:51 -0300

    chore: remove resolved TODO items from 2026-04-16 review doc

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./router";
 
 // biome-ignore lint/style/noNonNullAssertion: root element guaranteed by index.html
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <Router />
-  </StrictMode>
 );
