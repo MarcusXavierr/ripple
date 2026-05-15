759b745a09bdc22a94cde3fafefe90c9bb8f31b7
Marcus Xavier <marcusxavierr123@gmail.com>
2026-05-15 10:22:09 -0300

    chore: finishing extension

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from "wxt"
 
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  webExt: {
+    chromiumArgs: ["--lang=en-US"],
+  },
   manifest: ({ mode }) => {
-    const allowLocalhost = mode !== "production"
+    const isProduction = mode === "production"
+    const allowLocalhost = !isProduction
 
     return {
-      key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtRw/P41eQIElQOYeEgqMrTBrGIxEI1kq5OOr45j5nw2/S7nJM+hkL7u9DJT3HCxQryJpfFVmExWQHqxVx8GU0dZEn5ifVXqh0JH3hYfoCfJUfJ5f2rYjlVl0nChB/kvQ3h98A/ZVt37WMXdGEgTUztyoWQmSWodCxr6RnS7mDUnh5AzdU81N05jZd4IgimgLxJ5XKcIunTcruNyC43jQrzFrneeVoMOWUX6aHqLVapk7PB0rIUkNigpU9BM3txf7T9I8tLVEhG16XXE3NWhWtJuuw7elTZCvoyjElYubzPJuG4nwPoXIO+oAGvvpoP8Jgri6uESzxARyQMCHtzngwQIDAQAB",
+      ...(isProduction
+        ? {}
+        : {
+            key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtRw/P41eQIElQOYeEgqMrTBrGIxEI1kq5OOr45j5nw2/S7nJM+hkL7u9DJT3HCxQryJpfFVmExWQHqxVx8GU0dZEn5ifVXqh0JH3hYfoCfJUfJ5f2rYjlVl0nChB/kvQ3h98A/ZVt37WMXdGEgTUztyoWQmSWodCxr6RnS7mDUnh5AzdU81N05jZd4IgimgLxJ5XKcIunTcruNyC43jQrzFrneeVoMOWUX6aHqLVapk7PB0rIUkNigpU9BM3txf7T9I8tLVEhG16XXE3NWhWtJuuw7elTZCvoyjElYubzPJuG4nwPoXIO+oAGvvpoP8Jgri6uESzxARyQMCHtzngwQIDAQAB",
+          }),
       default_locale: "en",
       name: "__MSG_ext_name__",
       description: "__MSG_ext_description__",
